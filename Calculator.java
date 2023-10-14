add649ea00310d85d5995ca9954d6206666d9b97
SauravSaurav04 <sauravsaurav4444@gmail.com>
2023-10-14 11:57:24 +0530

    addition

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,9 @@
 public class Calculator {
+    static int add(int a, int b){
+        return a+b;
+    }
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println(add(2,3));
     }
 }
